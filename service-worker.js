2d3915b21aba1c765a42de2feb13d6cfbdc2bf3e
LinaDugau <dmitrie.alex201005@gmail.com>
2025-12-03 01:53:16 +0500

    update for ios

--- service-worker.js
+++ service-worker.js
@@ -10,8 +10,27 @@ const URLS = [
 // Установка SW и кеширование
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS).catch((err) => {
+        console.log("Ошибка кеширования:", err);
+      });
+    })
   );
+  self.skipWaiting();
+});
+
+// Активация SW
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 // Работа оффлайн
